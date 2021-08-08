ef886423c77f32ff2a3aa7abcf5c7a9dcf3c91a0
yacine <ramdaneyacine31@gmail.com>
2021-08-08 21:53:04 +0100

    day2

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,56 @@
 import Image from "next/image";
 import { SearchIcon } from "@heroicons/react/solid";
-function Header() {
+import { useState } from "react";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
+import { useRouter } from "next/dist/client/router";
+
+function Header({ placeholder }) {
+  const [searchInput, setSearchInpot] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [numberGuests, setNumberGuests] = useState(1);
+  const router = useRouter();
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const searchPage = () => {
+    router.push({
+      pathname: "/search",
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        numberGuests: numberGuests,
+      },
+    });
+  };
+
   return (
-    <div className="flex p-4 shadow-md justify-center items-center">
+    <header className="sticky top-0 z-50 bg-white grid grid-cols-4 shadow-md ">
       {/* left */}
-      <div className="items-center mr-auto flex">
+      <div
+        className=" relative items-center mr-auto flex p-5 md:px-8"
+        onClick={() => router.push("/")}
+      >
         <svg
           width="102"
           height="32"
           fill="currentcolor"
-          className="text-red-400 hidden sm:flex"
+          className="text-red-400 hidden sm:flex cursor-pointer"
         >
           <path d="M29.24 22.68c-.16-.39-.31-.8-.47-1.15l-.74-1.67-.03-.03c-2.2-4.8-4.55-9.68-7.04-14.48l-.1-.2c-.25-.47-.5-.99-.76-1.47-.32-.57-.63-1.18-1.14-1.76a5.3 5.3 0 00-8.2 0c-.47.58-.82 1.19-1.14 1.76-.25.52-.5 1.03-.76 1.5l-.1.2c-2.45 4.8-4.84 9.68-7.04 14.48l-.06.06c-.22.52-.48 1.06-.73 1.64-.16.35-.32.73-.48 1.15a6.8 6.8 0 007.2 9.23 8.38 8.38 0 003.18-1.1c1.3-.73 2.55-1.79 3.95-3.32 1.4 1.53 2.68 2.59 3.95 3.33A8.38 8.38 0 0022.75 32a6.79 6.79 0 006.75-5.83 5.94 5.94 0 00-.26-3.5zm-14.36 1.66c-1.72-2.2-2.84-4.22-3.22-5.95a5.2 5.2 0 01-.1-1.96c.07-.51.26-.96.52-1.34.6-.87 1.65-1.41 2.8-1.41a3.3 3.3 0 012.8 1.4c.26.4.45.84.51 1.35.1.58.06 1.25-.1 1.96-.38 1.7-1.5 3.74-3.21 5.95zm12.74 1.48a4.76 4.76 0 01-2.9 3.75c-.76.32-1.6.41-2.42.32-.8-.1-1.6-.36-2.42-.84a15.64 15.64 0 01-3.63-3.1c2.1-2.6 3.37-4.97 3.85-7.08.23-1 .26-1.9.16-2.73a5.53 5.53 0 00-.86-2.2 5.36 5.36 0 00-4.49-2.28c-1.85 0-3.5.86-4.5 2.27a5.18 5.18 0 00-.85 2.21c-.13.84-.1 1.77.16 2.73.48 2.11 1.78 4.51 3.85 7.1a14.33 14.33 0 01-3.63 3.12c-.83.48-1.62.73-2.42.83a4.76 4.76 0 01-5.32-4.07c-.1-.8-.03-1.6.29-2.5.1-.32.25-.64.41-1.02.22-.52.48-1.06.73-1.6l.04-.07c2.16-4.77 4.52-9.64 6.97-14.41l.1-.2c.25-.48.5-.99.76-1.47.26-.51.54-1 .9-1.4a3.32 3.32 0 015.09 0c.35.4.64.89.9 1.4.25.48.5 1 .76 1.47l.1.2c2.44 4.77 4.8 9.64 7 14.41l.03.03c.26.52.48 1.1.73 1.6.16.39.32.7.42 1.03.19.9.29 1.7.19 2.5zM41.54 24.12a5.02 5.02 0 01-3.95-1.83 6.55 6.55 0 01-1.6-4.48 6.96 6.96 0 011.66-4.58 5.3 5.3 0 014.08-1.86 4.3 4.3 0 013.7 1.92l.1-1.57h2.92V23.8h-2.93l-.1-1.76a4.52 4.52 0 01-3.88 2.08zm.76-2.88c.58 0 1.09-.16 1.57-.45.44-.32.8-.74 1.08-1.25.25-.51.38-1.12.38-1.8a3.42 3.42 0 00-1.47-3.04 2.95 2.95 0 00-3.12 0c-.44.32-.8.74-1.08 1.25a4.01 4.01 0 00-.38 1.8 3.42 3.42 0 001.47 3.04c.47.29.98.45 1.55.45zM53.45 8.46c0 .35-.06.67-.22.93-.16.25-.38.48-.67.64-.29.16-.6.22-.92.22-.32 0-.64-.06-.93-.22a1.84 1.84 0 01-.67-.64 1.82 1.82 0 01-.22-.93c0-.36.07-.68.22-.93.16-.3.39-.48.67-.64.29-.16.6-.23.93-.23a1.84 1.84 0 011.6.86 2 2 0 01.21.94zm-3.4 15.3V11.7h3.18v12.08h-3.19zm11.68-8.9v.04c-.15-.07-.35-.1-.5-.13-.2-.04-.36-.04-.55-.04-.89 0-1.56.26-2 .8-.48.55-.7 1.32-.7 2.31v5.93h-3.19V11.69h2.93l.1 1.83c.32-.64.7-1.12 1.24-1.48a3.1 3.1 0 011.81-.5c.23 0 .45.02.64.06.1.03.16.03.22.06v3.2zm1.28 8.9V6.74h3.18v6.5c.45-.58.96-1.03 1.6-1.38a5.02 5.02 0 016.08 1.31 6.55 6.55 0 011.6 4.49 6.96 6.96 0 01-1.66 4.58 5.3 5.3 0 01-4.08 1.86 4.3 4.3 0 01-3.7-1.92l-.1 1.57-2.92.03zm6.15-2.52c.57 0 1.08-.16 1.56-.45.44-.32.8-.74 1.08-1.25.26-.51.38-1.12.38-1.8 0-.67-.12-1.28-.38-1.79a3.75 3.75 0 00-1.08-1.25 2.95 2.95 0 00-3.12 0c-.45.32-.8.74-1.09 1.25a4.01 4.01 0 00-.38 1.8 3.42 3.42 0 001.47 3.04c.47.29.98.45 1.56.45zm7.51 2.53V11.69h2.93l.1 1.57a3.96 3.96 0 013.54-1.89 4.1 4.1 0 013.82 2.44c.35.76.54 1.7.54 2.75v7.24h-3.19v-6.82c0-.84-.19-1.5-.57-1.99-.38-.48-.9-.74-1.56-.74-.48 0-.9.1-1.27.32-.35.23-.64.52-.86.93a2.7 2.7 0 00-.32 1.35v6.92h-3.16zm12.52 0V6.73h3.19v6.5a4.67 4.67 0 013.73-1.89 5.02 5.02 0 013.95 1.83 6.57 6.57 0 011.59 4.48 6.95 6.95 0 01-1.66 4.58 5.3 5.3 0 01-4.08 1.86 4.3 4.3 0 01-3.7-1.92l-.09 1.57-2.93.03zm6.18-2.53c.58 0 1.09-.16 1.56-.45.45-.32.8-.74 1.09-1.25.25-.51.38-1.12.38-1.8a3.42 3.42 0 00-1.47-3.04 2.95 2.95 0 00-3.12 0c-.44.32-.8.74-1.08 1.25a3.63 3.63 0 00-.38 1.8 3.42 3.42 0 001.47 3.04c.47.29.95.45 1.55.45z"></path>
         </svg>
-
         <svg
           width="30"
           height="32"
@@ -25,19 +62,23 @@ function Header() {
       </div>
 
       {/* midlle */}
-      <div className="flex flex-grow mx-2 sm:mx-4 md:mx-16 xl:mx-32 2xl:mx-64 items-center md:border-2 rounded-full py-2 md:shadow-sm text-gray-400">
+      <div className="col-span-2 my-3 flex items-center md:border-2 rounded-full md:shadow-sm text-gray-400">
         <input
-          className="flex-grow pl-5 bg-transparent outline-none"
+          className="flex-grow pl-5  bg-transparent outline-none"
           type="text"
-          placeholder="start your search"
+          value={searchInput}
+          onChange={(e) => setSearchInpot(e.target.value)}
+          placeholder={placeholder || "start your search"}
         />
 
-        <SearchIcon className="hidden md:flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
+        <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
 
       {/* right */}
-      <div className="flex content-end text-gray-500 items-center space-x-4 ml-auto">
-        <h3 className="font-semibold text-s cursor-pointer">Become A Host</h3>
+      <div className=" relative flex justify-end text-gray-500 items-center space-x-3">
+        <h3 className="font-semibold text-s cursor-pointer hidden md:inline">
+          Become A Host
+        </h3>
 
         <div className="cursor-pointer h-6 w-6">
           <svg
@@ -56,7 +97,7 @@ function Header() {
           </svg>
         </div>
 
-        <div className="flex items-center space-x-4 rounded-full text-gray-700 border-2 border-gray-400 p-2">
+        <div className="flex items-center space-x-3 rounded-full text-gray-700 border-2 border-gray-400 p-2">
           <div className="h-5 w-5 cursor-pointer">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -87,7 +128,52 @@ function Header() {
           </div>
         </div>
       </div>
-    </div>
+
+      {searchInput && (
+        <div className="flex flex-col col-span-4 mx-auto mb-4">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5D61"]}
+            onChange={handleSelect}
+          />
+          <div className="flex items-center mb-4">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number Of Guests
+            </h2>
+            <div className="flex mr-2 items-center space-x-1">
+              <svg
+                className="text-red-500 h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
+              </svg>
+
+              <input
+                type="number"
+                value={numberGuests}
+                onChange={(e) => setNumberGuests(e.target.value)}
+                min="1"
+                className="w-10 text-lg font-medium outline-none"
+              />
+            </div>
+          </div>
+          <div className="flex">
+            <button
+              onClick={(e) => setSearchInpot("")}
+              className="flex-grow text-gray-500"
+            >
+              cancel
+            </button>
+            <button onClick={searchPage} className="flex-grow text-red-500">
+              search
+            </button>
+          </div>
+        </div>
+      )}
+    </header>
   );
 }
 
---
+++ pages/search.js
@@ -0,0 +1,72 @@
+import { format } from "date-fns";
+import { useRouter } from "next/dist/client/router";
+import Fotter from "../components/Fotter";
+import Header from "../components/Header";
+import InfoCard from "../components/InfoCard";
+
+function search({ searchResults }) {
+  const router = useRouter();
+  console.log({ searchResults });
+
+  const { location, startDate, endDate, numberGuests } = router.query;
+  const formattedStartdate = format(new Date(startDate), "dd MMM yy");
+  const formattedEndDate = format(new Date(endDate), "dd MMM yy");
+  const range = `${formattedStartdate} - ${formattedEndDate}`;
+
+  return (
+    <div>
+      <Header placeholder={`${location} | ${range} | ${numberGuests} guests`} />
+
+      <main className="flex">
+        <section className="flex-grow pt-14 px-6">
+          <p className="text-xs">
+            300+ stays -{range} - for {numberGuests} guests
+          </p>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stay in {location}
+          </h1>
+          <div className="hidden md:inline-flex mb-5 space-x-2 text-gray-800 whitespace-nowrap">
+            <p className="button">yoyo </p>
+            <p className="button">type of places</p>
+            <p className="button">yoyo yacine</p>
+            <p className="button">price</p>
+          </div>
+
+          <div className="flex flex-col">
+            {searchResults.map(
+              ({ img, location, title, description, star, price, total }) => (
+                <InfoCard
+                  img={img}
+                  location={location}
+                  title={title}
+                  description={description}
+                  star={star}
+                  price={price}
+                  total={total}
+                />
+              )
+            )}
+          </div>
+        </section>
+      </main>
+
+      <Fotter />
+    </div>
+  );
+}
+
+export default search;
+
+export async function getServerSideProps() {
+  console.log("before await ");
+
+  const searchResults = await fetch("https://jsonkeeper.com/b/5NPS").then(
+    (res) => res.json()
+  );
+  console.log({ searchResults });
+  return {
+    props: {
+      searchResults,
+    },
+  };
+}
